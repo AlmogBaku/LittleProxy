675d081c0c69f97ef20b0bab4cc5565082befed1
AlmogBaku <almog.baku@gmail.com>
2017-06-09 18:02:38 +0300

    upgrade dependencies

--- src/main/java/org/littleshoot/proxy/impl/ProxyToServerConnection.java
+++ src/main/java/org/littleshoot/proxy/impl/ProxyToServerConnection.java
@@ -592,7 +592,7 @@ public class ProxyToServerConnection extends ProxyConnection<HttpResponse> {
                             .serverSslEngine()));
                 } else {
                     connectionFlow.then(serverConnection.EncryptChannel(proxyServer.getMitmManager()
-                            .serverSslEngine(parsedHostAndPort.getHostText(), parsedHostAndPort.getPort())));
+                            .serverSslEngine(parsedHostAndPort.getHost(), parsedHostAndPort.getPort())));
                 }
 
             	connectionFlow
@@ -967,7 +967,7 @@ public class ProxyToServerConnection extends ProxyConnection<HttpResponse> {
             throw new UnknownHostException(hostAndPort);
         }
 
-        String host = parsedHostAndPort.getHostText();
+        String host = parsedHostAndPort.getHost();
         int port = parsedHostAndPort.getPortOrDefault(80);
 
         return proxyServer.getServerResolver().resolve(host, port);
